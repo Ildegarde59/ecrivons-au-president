75114b9eda053a21f833ee4dadfb85519a43b4fc
Ildegarde59 <ildegarde59@mailo.com>
2020-04-14 19:08:40 +0200

    Divers détails peaufinés

--- a-propos.php
+++ a-propos.php
@@ -16,6 +16,22 @@
 		?>
     <h1>À propos de nous</h1>
 		<article>
+      <section>
+        <h2>Qui sommes-nous&nbsp;?</h2>
+        <p>Nous sommes un groupe de jeunes apolitisé et sans statut légal qui ont décidé d’écrire une lettre au président pour peser sur les décisions prises par celui-ci.</p>
+        <h3>Initiateurs du projet&nbsp;:</h3>
+          <ul>
+            <li><a href="https://twitter.com/bastientrehoux" target="_blank" title="Compte Twitter de Bastien Tréhoux"><i class="fa fa-twitter" aria-hidden="true"></i> Bastien Tréhoux <i class="fa fa-external-link" aria-hidden="true"></i></a>, lycéen, Hauts-de-France</li>
+          </ul>
+      </section>
+      <section>
+        <h2>Nous contacter</h2>
+        <p>Vous pouvez nous contacter à l’adresse e-mail suivante&nbsp;: <a href="mailto:?to=contact@ecrivonsaupresident.yn.fr" title="Envoyer un mail">contact@ecrivonsaupresident.yn.fr</a></p>
+      </section>
+      <section>
+        <h2>Réutilisation du contenu</h2>
+        <p>Le contenu de ce site est librement réutilisable selon les termes de la <a title="Consulter la licence" target="_blank" rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/deed.fr">licence Creative Commons Attribution - Pas d’utilisation commerciale 4.0 International</a>. Le code source du site est quant à lui disponible sous licence MIT sur notre dép <a href="https://github.com/Ildegarde59/ecrivons-au-president" title="Accéder au dépôt Git" target="_blank">Git.</p>
+      </section>
     </article>
 		<?php /* Footer */ include "includes/footer.php" ; ?>
 </body>
--- confidentialite.php
+++ confidentialite.php
@@ -18,18 +18,18 @@
 		<article>
       <p>Nous tenons à être transparent quant à la collecte de données réalisées sur ce site. C’est pourquoi nous allons les détailler ici.</p>
       <section>
-        <h2>Services tiers utilisés sur ce site</h2>
+        <h2>Services tiers utilisés sur ce site utilisant des cookies</h2>
         <ul>
           <li><strong>Google Fonts&nbsp;:</strong> permet d’afficher une police de caractères personnalisée sur le site &mdash; ne collecte pas de données&nbsp;;</li>
           <!--<li><strong>Google Analytics&nbsp;:</strong> permet d’effectuer des statistiques de mesures d’audiences, basées sur les données recueillies par le moteur de recherche Google &mdash; collecte des données personnelles&nbsp;;</li>-->
           <li><strong>Google Forms&nbsp;:</strong> permet de créer un formulaire à compléter (ici utilisé pour recueillir les signatures) &mdash; collecte des données personnelles&nbsp;;</li>
           <li><strong>ForkAwesome&nbsp;:</strong> affiche des icônes sur le site &mdash; ne collecte pas de données&nbsp;;</strong></li>
-          <li><strong>CookieConsent by Insites</strong>&nbsp;: préviend les utilisteurs de l’utilisation de cookies &mdash; ne collecte pas de données.</li>
+          <li><strong>CookieConsent by Insites</strong>&nbsp;: prévient les utilisteurs de l’utilisation de cookies &mdash; ne collecte pas de données.</li>
         </ul>
       </section>
       <section>
         <h2>Utilisation des données personnelles</h2>
-        <p>L’utilisation des services de Google sont soumis à politique de confidentialité de cette entreprise. Les données personnelles recueillies via le formulaire Google Forms ne seront pas cédées et seront supprimées dès que nous aurons pris connaissance de la réception de la lettre par Élysée ou dans un délai de trois mois après l’envoi de la lettre si nous ne recevons pas de réponde.</p>
+        <p>L’utilisation des services de Google sont soumis à politique de confidentialité de cette entreprise. Les données personnelles recueillies via le formulaire Google Forms ne seront pas cédées et seront supprimées dès que nous aurons pris connaissance de la réception de la lettre par l’Élysée ou dans un délai de trois mois après l’envoi de la lettre si nous ne recevons pas de réponse.</p>
       </section>
       <section>
         <h2>Des questions</h2>
